1823b6b62ca00704af961baa0c185407e793a01f
Кот-кайфун Новый <Vladimir_225meil.ru@mail.ru>
2024-04-26 18:15:08 +0300

    Добавил центрирование хэдэра

---
+++ BasketballProj/Controllers/AdminPagesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasketballProj.Controllers
+{
+    public class AdminPagesController : Controller
+    {
+        public IActionResult EventAdPage() => View();
+        public IActionResult MatchupsManaging() => View();
+        public IActionResult PlayersManaging() => View();
+        public IActionResult SeasonsManaging() => View();
+        public IActionResult TeamsManaging() => View();
+
+
+        public IActionResult TechnicalAdPage() => View();
+        public IActionResult TeamsReportsManaging() => View();
+        public IActionResult ExecutionsManaging() => View();
+
+    }
+}
+
--- BasketballProj/Controllers/VisitorPagesController.cs
+++ BasketballProj/Controllers/VisitorPagesController.cs
@@ -5,8 +5,11 @@ namespace BasketballProj.Controllers
     public class VisitorPagesController : Controller
     {
         public IActionResult Matchups() => View();
+        public IActionResult MatchupInfo() => View();
         public IActionResult Photos() => View();
         public IActionResult Players() => View();
+        public IActionResult PlayerInfo() => View();
         public IActionResult Teams() => View();
+        public IActionResult TeamInfo() => View();
     }
 }
